7d7c52c3142c43d4ea2f8b328266870cd4674a5d
Ahmed <ahmedmahmoudkzm@gmail.com>
2023-05-21 20:35:10 +0200

    finished home page

--- app/index.js
+++ app/index.js
@@ -10,6 +10,10 @@ let loggedIn = false
 
 const main = document.getElementById('main')
 const loginBtn = document.getElementById('login-btn')
+const logoDiv = document.getElementById('logo-div')
+const footerLogo = document.getElementById('footer-logo')
+const applyBtn = document.getElementById('apply-btn')
+const learnMoreBtn = document.getElementById('learn-more-btn')
 
 // ----------------
 // Functions
@@ -18,13 +22,13 @@ const loginBtn = document.getElementById('login-btn')
 function addField(form, title, type, invalidMsg, placeholder = '') {
     const label = document.createElement('label')
     const input = document.createElement('input')
-    const id = title.toLowerCase().replace(/\s+/g, '-');
+    const id = title.toLowerCase().replace(/\s+/g, '-')
 
     label.setAttribute('for', id)
     label.textContent = title
 
     input.setAttribute('type', type)
-    input.setAttribute('oninput', 'setCustomValidity(\'\')')
+    input.setAttribute('oninput', "setCustomValidity('')")
     input.setAttribute('oninvalid', `this.setCustomValidity(${invalidMsg})`)
     if (type !== 'date') {
         input.setAttribute('placeholder', placeholder)
@@ -32,14 +36,17 @@ function addField(form, title, type, invalidMsg, placeholder = '') {
     if (type === 'tel') {
         input.setAttribute('placeholder', '+201234567890')
     }
-    input.required = true;       
+    input.required = true
     input.id = id
-    
+
     if (type === 'email') {
-        input.setAttribute('pattern', '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')
+        input.setAttribute('pattern', '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$')
     } else if (type === 'password') {
         input.setAttribute('minlength', 8)
-        input.setAttribute('pattern', '^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\\-=[\]{};\':\"\\|,.<>/?]).+$')
+        input.setAttribute(
+            'pattern',
+            '^(?=.*[A-Z])(?=.*[a-z])(?=.*d)(?=.*[!@#$%^&*()_+\\-=[]{};\':"\\|,.<>/?]).+$'
+        )
     } else if (type === 'text') {
         input.setAttribute('minlength', 3)
         input.setAttribute('maxlength', 64)
@@ -48,13 +55,19 @@ function addField(form, title, type, invalidMsg, placeholder = '') {
         input.setAttribute('maxlength', 15)
         input.setAttribute('pattern', '^(010|011|012|015)[0-9]{8}$')
     } else if (type === 'date') {
-        const today = new Date();
-        const maxDate = today.toISOString().slice(0, 10);
-        const minDate = new Date(today.getFullYear() - 21, today.getMonth(), today.getDate()).toISOString().slice(0, 10);
+        const today = new Date()
+        const maxDate = today.toISOString().slice(0, 10)
+        const minDate = new Date(
+            today.getFullYear() - 21,
+            today.getMonth(),
+            today.getDate()
+        )
+            .toISOString()
+            .slice(0, 10)
         input.setAttribute('min', minDate)
         input.setAttribute('max', maxDate)
     } else if (type === 'radio') {
-	input.setAttribute('value', 'national-id')
+        input.setAttribute('value', 'national-id')
     }
     form.append(label)
     form.append(input)
@@ -71,12 +84,34 @@ function createRegisterForm() {
     btn.setAttribute('type', 'submit')
     btn.setAttribute('form', 'registerForm')
 
-
-    addField(form, 'Email', 'email', 'Email should contain \'@\'', 'user@mail.example')
-    addField(form, 'Password', 'password', 'Password should contain at least 8 characters including a symbol, at least one lowercase and one uppercase characters and at least one number', '********')
-    addField(form, 'Confirm Password', 'password', 'Passwords does not match', '********')
+    addField(
+        form,
+        'Email',
+        'email',
+        "Email should contain '@'",
+        'user@mail.example'
+    )
+    addField(
+        form,
+        'Password',
+        'password',
+        'Password should contain at least 8 characters including a symbol, at least one lowercase and one uppercase characters and at least one number',
+        '********'
+    )
+    addField(
+        form,
+        'Confirm Password',
+        'password',
+        'Passwords does not match',
+        '********'
+    )
     addField(form, 'Phone Number', 'tel', 'Number should start with +20')
-    addField(form, 'Date of Birth', 'date', 'You must be at least 21 years old to register')
+    addField(
+        form,
+        'Date of Birth',
+        'date',
+        'You must be at least 21 years old to register'
+    )
     addField(form, 'National ID', 'radio')
     addField(form, 'Passport Number', 'radio')
     addField(form, 'National ID / Passport Number', 'text')
@@ -96,8 +131,13 @@ function createLoginForm() {
     btn.setAttribute('type', 'submit')
     btn.setAttribute('form', 'registerForm')
 
-
-    addField(form, 'Email', 'email', 'This field is required', 'user@mail.example')
+    addField(
+        form,
+        'Email',
+        'email',
+        'This field is required',
+        'user@mail.example'
+    )
     addField(form, 'Password', 'password', 'This field is required', '********')
 
     form.append(btn)
@@ -123,9 +163,7 @@ function createForgotPasswordForm() {
 
 function populateHome() {
     if (loggedIn) {
-
     } else {
-        
     }
 }
 
@@ -134,16 +172,40 @@ function populateHome() {
 // ----------------
 
 loginBtn.addEventListener('click', () => {
-    
+    window.location.href = './pages/login.html'
+})
+
+logoDiv.addEventListener('click', () => {
+    if (window.location.href.split('/').pop() == 'home.html') {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    } else {
+        window.location.href = './home.html'
+    }
+})
+
+footerLogo.addEventListener('click', () => {
+    if (window.location.href.split('/').pop() == 'home.html') {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    } else {
+        window.location.href = './home.html'
+    }
+})
+
+applyBtn.addEventListener('click', () => {
+    window.location.href = './pages/registerPage.html'
+})
+
+learnMoreBtn.addEventListener('click', () => {
+    window.location.href = './pages/learn-more.html'
 })
 
 // ----------------
 // Copyright year
 // ----------------
 
-let currentYear = new Date().getFullYear();
-let startYr = document.getElementById('starting-year').textContent;
+let currentYear = new Date().getFullYear()
+let startYr = document.getElementById('starting-year').textContent
 
 if (startYr != currentYear) {
-    document.getElementById('current-year').textContent = `-${currentYear}`;
+    document.getElementById('current-year').textContent = `-${currentYear}`
 }
