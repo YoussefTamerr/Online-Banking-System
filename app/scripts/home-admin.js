ea19edd1d6a3096f1204138b749203d3c42f69ab
ibrahim <ibrahim@example.com>
2023-05-25 15:46:55 +0300

    Added admin page technical issues

--- app/scripts/home-admin.js
+++ app/scripts/home-admin.js
@@ -1,12 +1,4 @@
-// ----------------
-// Global Variables
-// ----------------
-
 const logoutBtn = document.getElementById('login-btn')
-
-// ----------------
-// Functions
-// ----------------
 
 function resetLocalStorage() {
     localStorage.setItem('email', JSON.stringify(''))
@@ -19,8 +11,23 @@ function logout() {
     localStorage.setItem('fname', JSON.stringify('Guest'))
 }
 
-// ----------------
-// Event Listeners
-// ----------------
-
 logoutBtn.addEventListener('click', logout)
+
+const issues = document.querySelectorAll(".issue");
+
+issues.forEach(element => {
+    element.querySelector(".mark").addEventListener('click', (e) => {
+        e.preventDefault();
+        if (e.target.hasAttribute("checked")) {
+            e.target.removeAttribute("checked");
+            e.target.parentNode.style.textDecoration = "none";
+            e.target.parentNode.style.color = "#313945";
+        } else {
+            e.target.setAttribute("checked", "true");
+            console.log(e.target.parent)
+            e.target.parentNode.style.textDecoration = "line-through";
+            e.target.parentNode.style.color = "grey";
+
+        }
+    });
+});
