02f8392090944352f92abff2df2477b88aa09833
KarimElhaggar <127612223+KarimElhaggar@users.noreply.github.com>
2023-05-29 14:19:49 +0300

    added close and open pages, added transfer money page

---
+++ app/scripts/close-account.js
@@ -0,0 +1,73 @@
+// ----------------
+// Global Variables
+// ----------------
+
+let incorrectFlag = false
+
+// ----------------
+// DOM Elements
+// ----------------
+
+const registerForm = document.getElementById('register-form')
+const password = document.getElementById('password-input')
+const rsn = document.getElementById('rsn')
+const registerBtn = document.getElementById('register-form-btn')
+const logoutBtn = document.getElementById('login-btn')
+
+// ----------------
+// Local Storage
+// ----------------
+
+// ----------------
+// Functions
+// ----------------
+
+function register() {
+    if (password.reportValidity()) {
+        if (password.value === 'letmetellyousomething') {
+            registerBtn.textContent = "Request Submitted";
+            registerBtn.style.backgroundColor = "limegreen";
+            registerBtn.classList.add('no-hover')
+            registerBtn.disabled = true;
+            incorrectFlag = false
+            document.getElementById('err-msg').remove()
+            registerForm.reset();
+        } else {
+            incorrectInfo()
+        }
+        password.value = ''
+    }
+}
+
+function incorrectInfo() {
+    const err = document.createElement('p')
+    err.id = 'err-msg'
+    err.textContent = `Incorrect Password.`
+    if (!incorrectFlag) {
+        insertAfter(document.getElementById('btn-cont'), err)
+        incorrectFlag = true
+    }
+}
+
+function logout() {
+    window.location.href = './login.html'
+    localStorage.setItem('fname', JSON.stringify('Guest'))
+}
+
+// ----------------
+// Self-Invoking Function
+// ----------------
+
+// ----------------
+// Event Listeners
+// ----------------
+
+logoutBtn.addEventListener('click', logout)
+registerBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    register()
+})
+
+// ----------------
+// Function Calls
+// ----------------
---
+++ app/scripts/transfer-money.js
@@ -0,0 +1,74 @@
+// ----------------
+// Global Variables
+// ----------------
+
+let incorrectFlag = false
+
+// ----------------
+// DOM Elements
+// ----------------
+
+const registerForm = document.getElementById('register-form')
+const password = document.getElementById('password-input')
+const num = document.getElementById('account-number')
+const amount = document.getElementById('amount-input')
+const registerBtn = document.getElementById('register-form-btn')
+const logoutBtn = document.getElementById('login-btn')
+
+// ----------------
+// Local Storage
+// ----------------
+
+// ----------------
+// Functions
+// ----------------
+
+function register() {
+    if (password.reportValidity() && num.reportValidity() && amount.reportValidity()) {
+        if (password.value === 'letmetellyousomething') {
+            registerBtn.textContent = "Application Submitted";
+            registerBtn.style.backgroundColor = "limegreen";
+            registerBtn.classList.add('no-hover')
+            registerBtn.disabled = true;
+            incorrectFlag = false
+            document.getElementById('err-msg').remove()
+            registerForm.reset()
+        } else {
+            incorrectInfo()
+        }
+        password.value = ''
+    }
+}
+
+function incorrectInfo() {
+    const err = document.createElement('p')
+    err.id = 'err-msg'
+    err.textContent = `Incorrect Password.`
+    if (!incorrectFlag) {
+        insertAfter(document.getElementById('btn-cont'), err)
+        incorrectFlag = true
+    }
+}
+
+function logout() {
+    window.location.href = './login.html'
+    localStorage.setItem('fname', JSON.stringify('Guest'))
+}
+
+// ----------------
+// Self-Invoking Function
+// ----------------
+
+// ----------------
+// Event Listeners
+// ----------------
+
+logoutBtn.addEventListener('click', logout)
+registerBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    register()
+})
+
+// ----------------
+// Function Calls
+// ----------------
